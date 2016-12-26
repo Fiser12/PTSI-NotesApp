8e8f3b81df5d48780d1cae428982a35b3fd35114
Fiser <fiser12@icloud.com>
2016-12-26 15:37:18 +0100

    Cambios en el cliente

--- FiServer-Angular/app/app.js
+++ FiServer-Angular/app/app.js
@@ -1,15 +1,22 @@
-'use strict';
+var myApp = angular.module('myApp',[]).controller("myControllerContent",function($scope, $http) {
+    $scope.etiquetas=[{nombre:"Universidad"},{nombre:"Personal"}];
+    $scope.nota = {titulo:"Titulo", nota:"Nota", etiquetas:["Personal", " Universidad"]}
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.todasLasNotas',
-  'myApp.favoritas',
-  'myApp.compartidas',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/todasLasNotas'});
-}]);
-// Enable pusher logging - don't include this in production
+    $http.get('http://localhost:4000/api/nota/list', {headers: { 'Content-Type': 'application/json', 'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiRmlzZXIxMiIsInBhc3N3b3JkIjoiJDJhJDEwJEZKUXk3SnY3a3h2VUZzVExtUkJaYy5yZ1BlMVZFQVBRNlczSnJJLlBpS3pCaE5WU0dhTWJ1IiwiX2lkIjoiNTc1Y2M1NWJmZjA3ZDc3ZjExYzY1MTkzIiwiX192IjowfQ.zVl8hdaxO2wmtqERiBJeu705p6xAKtoe5Oz6ApXIMGY' }}).success(function(data) {
+        $scope.notas = data;
+        console.log(data)
+    }).error(function(data) {
+        console.log('Error: ' + data);
+    });
+
+});
+
+$(document).ready(function () {
+    $('#bootstrapTagsInputForm')
+        .find('[name="tags"]')
+        // Revalidate the cities field when it is changed
+        .change(function (e) {
+            $('#bootstrapTagsInputForm').formValidation('revalidateField', 'cities');
+        })
+        .end()
+});
